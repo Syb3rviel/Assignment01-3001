2a9e2999c42ccd9b91c00242e0c426546fa851db
Syber <99502867+Syb3rviel@users.noreply.github.com>
2025-01-25 15:04:19 -0700

    scripts created -- base made

---
+++ Assignment01/Assets/Assets/Scripts/SnapToCursor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//this script is for testing purposes --- after i finalize the rest of my scripts i shall be finished with this and work on the random drop in instantiation
+//during this they will also probably not have limits of where they cant go so the flee option he will just run and disapear for now
+public class SnapToCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 mouse = Input.mousePosition;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        Debug.Log(mouse);
+
+        transform.position = mouse;
+    }
+}
---
+++ Assignment01/Assets/Assets/Scripts/Steering.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Steering
+{
+    //all references will be made in the gamemanager to both connect the seeker and target together and call the below fucntions when needed
+
+    public static Vector3 SeekBehaviour()
+    {
+        return Vector3.zero;
+    }
+
+    public static Vector3 FleeBehaviour()
+    {
+        return Vector3.zero;
+    }
+
+    public static Vector3 ArriveBehaviour()
+    {
+        return Vector3.zero;
+    }
+
+    public static Vector3 AvoidBehaviour()
+    {
+        return Vector3.zero;
+    }
+}
